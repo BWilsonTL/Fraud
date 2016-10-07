96b23087aa6ef346e8d3f2bd9c9c20f32b7028e3
bewilson <bewilson@ruelala.com>
2016-10-07 16:34:21 -0400

    Finished the member data set for predictor variables.

--- chargefraud.py
+++ chargefraud.py
@@ -81,8 +81,7 @@ age_dict = {
 }
 
 
-def data_prep():
-
+def member_prep():
     todaydate = datetime.date.today()
     # import the static table
     fraud_import = static_join(f_path, e_path)
@@ -92,18 +91,18 @@ def data_prep():
     # query snowflake for member attributes
     member_attributes_raw = sf.SnowConnect(mbr_attr_qry, sf_uname, sf_pwd,
                                        'ADW_QA_DB', 'ADM', 'ADW_QA_QRY_RL', 'QUERY_WH').execute_query()
-
     # create features in member_attributes
     member_attributes = member_attributes_raw.copy()
-
+    # cleanup the login count field
+    member_attributes = fp.df_field_fill_clean(member_attributes, 'LOGIN_CNT', 0)
     # create bool column for email subscribe
     member_attributes['EMAIL_SUB'] = (member_attributes['EMAIL_UNSUB_DT'] > todaydate).astype(int)
     member_attributes.drop('EMAIL_UNSUB_DT', axis=1, inplace=True)
-
     # Get the total shipped order qty for the member
     member_attributes['Effective_Order_QTY'] = (member_attributes['ORDER_QTY']
                                                 - member_attributes['CANCEL_QTY']
                                                 - member_attributes['RETURN_QTY'])
+
     member_attributes['Effective_Order_AMT'] = (member_attributes['ORDER_AMT']
                                                 - member_attributes['CANCEL_AMT']
                                                 - member_attributes['RETURN_AMT'])
@@ -122,33 +121,41 @@ def data_prep():
                  member_filter['LAST_LOGIN_DT'], member_filter['JOIN_DT'], 'D')
     ft.deltadate(member_filter, 'Account_Age', todaydate, member_filter['JOIN_DT'], 'D')
     ft.deltadate(member_filter, 'recency', todaydate, member_filter['LAST_LOGIN_DT'], 'D')
-
     # drop the datetime fields
     date_fields = ['LAST_LOGIN_DT', 'JOIN_DT']
     for i in date_fields:
         member_filter.drop(i, axis=1, inplace=True)
-
     # basis expansion of grouped categorical data
     base_list = ['MARITAL_STATUS', 'Ages', 'LOYALTY_TIER_NM', 'GENDER_CD', 'COUNTRY']
     for i in base_list:
         ff.basis_expansion(member_filter, i)
-
     # bin expansion of continuous data
-    be_list = [['Active_Age_Days', 4], ['Account_Age', 5], ['recency', 8], ['LOGIN_CNT', 5]]
+    be_list = [['Active_Age_Days', 4, 'auto'], ['Account_Age', 5, 'auto'], ['recency', 4, 'auto'],
+               ['LOGIN_CNT', 5, 'auto']]
     for i in be_list:
-        ft.bin_expansion(member_filter, i[0], ft.bin_generator(i[1]), source_del=True)
-
+        ft.bin_expansion(member_filter, i[0], ft.bin_generator(i[1]), source_del=True, bin_scale=i[2])
     # do column standardization for some fields.
     scaler = preprocessing.MinMaxScaler()
     scaling_list = ['ORDER_QTY', 'CANCEL_QTY', 'RETURN_QTY', 'ORDER_AMT', 'CANCEL_AMT', 'RETURN_AMT',
                     'Effective_Order_QTY', 'Effective_Order_AMT']
     member_filter[scaling_list] = scaler.fit_transform(member_filter[scaling_list])
 
+    return member_filter
+
+member_data = member_prep()
+
+
+
+
+
+def session_prep():
+
     # get stats on activity for members
     session_activity_raw = sf.SnowConnect(session_activity_qry, sf_uname, sf_pwd,
                                       'ADW_PRD_DB', 'ADM', 'ADW_PRD_QRY_RL', 'QUERY_WH').execute_query()
 
     # create a copy
+    scaler = preprocessing.MinMaxScaler()
     session_activity = session_activity_raw.copy()
     session_list = ['BOUTIQUE_CNT', 'TOTAL_ACTIVITY']
     session_activity[session_list] = scaler.fit_transform(session_activity[session_list])
@@ -159,12 +166,13 @@ def data_prep():
 
 
     """
-    ggplot(aes(x='Account_Age'), data=member_attributes) + geom_histogram(binwidth=1)
+    ggplot(aes(x='LOGIN_CNT'), data=member_attributes) + geom_histogram()
     ggplot(member_attributes, aes(x='Active_Age_Days')) + geom_histogram()
     """
 
 
-
+def predictor_merge():
+    pass
 
 
 class Main(object):
@@ -173,21 +181,6 @@ class Main(object):
 
     def main(self):
         pass
-
-
-session_activity_qry = """
-SELECT
-    MAX(SESSION_SK) "SESSION_SK",
-    MIN(ACTIVITY_DT) as ACTIVITY_START,
-    MAX(ACTIVITY_DT) as ACTIVITY_END,
-    MAX(MEMBER_SK) "MEMBER_SK",
-    COUNT(BOUTIQUE_SK) "ITEM_VIEWS",
-    SUM(ACTIVITY_CNT) "TOTAL_ACTIVITY",
-    MAX(DEVICE_CATEGORY_ID) "DEVICE_ID"
-FROM A_BTQ_VISIT_HRLY
-GROUP BY A_BTQ_VISIT_HRLY.MEMBER_SK, A_BTQ_VISIT_HRLY.SESSION_SK
-"""
-
 
 
 
